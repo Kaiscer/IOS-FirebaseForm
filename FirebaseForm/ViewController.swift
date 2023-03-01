2117e94285afa90eb36688f07677e097668df8ae
Kai_EV <kasicer1@gmail.com>
2023-03-01 10:05:28 +0100

    Initial Commit

---
+++ FirebaseForm/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirebaseForm
+//
+//  Created by Kaiscer Vasquez on 1/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // MARK: Outlet
+
+    
+    @IBOutlet weak var tvExpect: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
