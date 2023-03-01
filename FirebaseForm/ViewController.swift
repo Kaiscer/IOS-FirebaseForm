a9fd87b712e293850caee65c81cf3327b0e059a3
Kai_EV <kasicer1@gmail.com>
2023-03-01 12:09:20 +0100

    save, see, delete on firebase

--- FirebaseForm/ViewController.swift
+++ FirebaseForm/ViewController.swift
@@ -24,20 +24,37 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+          
+              
        
     }
     
     // MARK: Button Actions
     
-    @IBAction func btnSave(_ sender: Any) {
+    @IBAction func btnSave(_ sender: UIButton) {
         
         db.collection("usuarios").document(tvExpect.text!).setData(["usuarios": tvName.text ?? "", "email":tvmail.text ?? ""])
+        
+        
     }
     
-    @IBAction func btnSearhc(_ sender: Any) {
+    @IBAction func btnSearhc(_ sender: UIButton) {
+        db.collection("usuarios").document(tvExpect.text!).getDocument{
+            (DocumentSnapshot, error) in
+            if let document = DocumentSnapshot, error == nil {
+                
+                self.tvName.text = document.get("usuario") as? String
+                self.tvmail.text = document.get("emial") as? String
+                
+            }
+        }
     }
     
-    @IBAction func btnDeleted(_ sender: Any) {
+    @IBAction func btnDeleted(_ sender: UIButton) {
+        db.collection("usuarios").document(tvExpect.text!).delete()
+        tvExpect.text = ""
+        tvName.text = ""
+        tvmail.text = ""
     }
 }
 
