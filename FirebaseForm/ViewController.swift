3c39c87b94a52525f762a0b2870a9284bd64a2c1
Kai_EV <kasicer1@gmail.com>
2023-03-01 11:13:41 +0100

    connection Firebase

--- FirebaseForm/ViewController.swift
+++ FirebaseForm/ViewController.swift
@@ -6,19 +6,38 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
-    // MARK: Outlet
+    // MARK: Outlets
 
     
     @IBOutlet weak var tvExpect: UITextField!
+    @IBOutlet weak var tvName: UITextField!
+    @IBOutlet weak var tvmail: UITextField!
+    
+
+    
+    private let db = Firestore.firestore()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
        
     }
-
-
+    
+    // MARK: Button Actions
+    
+    @IBAction func btnSave(_ sender: Any) {
+        
+        db.collection("usuarios").document(tvExpect.text!).setData(["usuarios": tvName.text ?? "", "email":tvmail.text ?? ""])
+    }
+    
+    @IBAction func btnSearhc(_ sender: Any) {
+    }
+    
+    @IBAction func btnDeleted(_ sender: Any) {
+    }
 }
 
